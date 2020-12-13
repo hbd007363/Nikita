799ec789c013af7f8a51bcce23acef1bf709d04c
hbd007363 <75947041+hbd007363@users.noreply.github.com>
2020-12-13 23:40:04 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'December 13, 2020',
+  birthdate: 'December 14, 2020',
   name: 'NIKITA'
 };
 
